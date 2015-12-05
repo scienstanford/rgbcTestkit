b689191b1e187c7b335bfbed8607ecfa68bc593f
Qiyuan Tian <fdqiyuantian@gmail.com>
2015-12-04 18:36:35 -0800

    Render stanford images using old codes

--- functions/rgbcRawHist.m
+++ functions/rgbcRawHist.m
@@ -2,5 +2,5 @@ function [pdf, vals] = rgbcRawHist(raw)
 % assume raw ranges [0, 255]
 vals = 0 : 255;
 pdf = hist(raw(:), vals);
-figure, bar(vals, pdf), xlim([0, 255])
+figure, bar(vals, pdf), xlim([0, 255]);
 end
--- scripts/s_rgbcRender.m
+++ scripts/s_rgbcRender.m
@@ -11,21 +11,24 @@ clear, clc, close all
 
 %% Initialize ISET
 s_initISET
- 
-%% Load camera
+
+%% Render Stanford raw
+% load camera
 fpCamera = fullfile(rgbcrootpath, 'data', 'camera');
-fnCamera = 'L3camera_rgbc-omv1.mat';
+fnCamera = 'L3camera_rgbc-omv2.mat';
 
 tmp = load(fullfile(fpCamera, fnCamera));
 camera = tmp.L3camera;
 
-%% Load omv raw images
-pathRaw = fullfile(rgbcrootpath, 'data', 'omv');
-files = dir(fullfile(pathRaw, '*RGBC*raw'));
-rawSz = [3264, 2448];
+% load stanford raw images from scarlet
+scarletpath = 'http://scarlet.stanford.edu/validation/SCIEN/L3/rgbc';
+nameScene = 'memchu3';
+pathRaw = fullfile(scarletpath, nameScene);
+files = lsScarlet(pathRaw, 'raw');
+rawSz = [4224, 3136];
 
-for ii = 1 %: length(files)
-    fnRaw = files(ii).name;
+for ii = 7%1 : length(files)
+    fnRaw = files(ii).name
     fpRaw = fullfile(pathRaw, fnRaw);
     
     % read a raw image
@@ -40,30 +43,41 @@ for ii = 1 %: length(files)
     raw = raw / max(raw(:));
     figure, imagesc(raw), axis image
     
-    [srgbL3, lrgbL3] = rgbcRender(raw, camera, fnRaw);
+    [srgbL3, lrgbL3] = rgbcRender(raw, camera, [nameScene '_' fnRaw]);
 end
 
 
-
-%% Load stanford raw images from scarlet
-pathRaw = 'http://scarlet.stanford.edu/validation/SCIEN/L3/rgbc/memchu2macbeth';
-files = lsScarlet(pathRaw, 'raw');
-rawSz = [4224, 3136];
-
-for ii = 1 %: length(files)
-    fnRaw = files(ii).name;
-    fpRaw = fullfile(pathRaw, fnRaw);
-    
-    % Read a raw image
-    raw = rgbcRawRead(fpRaw, rawSz);
-    raw = raw / max(raw(:));
-    figure, imagesc(raw), axis image
-    
-%     [srgbL3, lrgbL3] = rgbcRender(fpRaw, camera);
-end
-
-
-
+%% Render OMV raw
+% % load camera
+% fpCamera = fullfile(rgbcrootpath, 'data', 'camera');
+% fnCamera = 'L3camera_rgbc-omv1.mat';
+% 
+% tmp = load(fullfile(fpCamera, fnCamera));
+% camera = tmp.L3camera;
+% 
+% % load omv raw images
+% pathRaw = fullfile(rgbcrootpath, 'data', 'omv');
+% files = dir(fullfile(pathRaw, '*RGBC*raw'));
+% rawSz = [3264, 2448];
+% 
+% for ii = 1 %: length(files)
+%     fnRaw = files(ii).name;
+%     fpRaw = fullfile(pathRaw, fnRaw);
+%     
+%     % read a raw image
+%     raw = rgbcRawRead(fpRaw, rawSz);
+%     
+%     % pixel value hist
+%     rgbcRawHist(raw)
+%     offset = 3;
+%     
+%     raw = raw - offset; % remove offset
+%     raw(raw <= 0) = 0;
+%     raw = raw / max(raw(:));
+%     figure, imagesc(raw), axis image
+%     
+%     [srgbL3, lrgbL3] = rgbcRender(raw, camera, fnRaw);
+% end
 
 
 
