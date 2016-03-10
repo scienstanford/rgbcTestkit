0c943d96ee069e5417bb8b2ede19616df840d15a
qytian <qytian@stanford.edu>
2016-03-09 18:25:46 -0800

    Minor edits on the comments on the s_rgbcCfaCreate.m scripts

--- scripts/s_rgbcCfaCreate.m
+++ scripts/s_rgbcCfaCreate.m
@@ -1,7 +1,7 @@
-% s_rgbccfaCreat.m
+% s_rgbcCfaCreat.m
 %
 % This script creates RGBC CFA using the QE and the spatial layout provided
-% by Omnivision. The documentes are included in the "documents" directory. 
+% by Omnivision. The documentes are included in the "docs" directory. 
 % 
 % Note there might be flip, rotation and shift for the CFA repetition block
 % to align with raw image. This is because the RGBC has high resolution and
@@ -9,17 +9,18 @@
 % high bandwidth reading raw images from the testkit, which is especially
 % an issue when using the RGBC testkit to capture videos. 
 %
-% Run script s_rgbccfaAlignCheck.m determine how the CFA block should be
-% modified.
+% Run script s_rgbcCfaAlignCheck.m to determine how the CFA block should be
+% flipped and shifted. 
 %
-% See also s_rgbcCfaAlignCheck.m
+% See also 
+%   s_rgbcCfaAlignCheck.m
 % 
 % QT (c) Stanford VISTA LAB, 2015
 clear, clc, close all
 
-%%
+%% Read QE curves from provided spreadsheet named below
 fn = '1.4um RGBC QE.xlsx';
-fp = fullfile(rgbcrootpath, 'documents', fn);
+fp = fullfile(rgbcrootpath, 'docs', fn);
 
 [num, txt, raw] = xlsread(fp);
 wavelength = num(:, 1);
@@ -28,6 +29,7 @@ g = num(:, 3);
 b = num(:, 4);
 w = num(:, 5);
 
+%% Plot QE curves
 figure
 hold on
 plot(wavelength, r, 'r');
@@ -39,6 +41,8 @@ xlabel('wavelength (nm)');
 ylabel('sensitivities');
 saveas(gcf, fullfile(rgbcrootpath, 'data', 'cfa', 'QE.png'));
 
+%% CFA0: repetion block from provided document 
+% Refer to docs/Gen2 readme.docx 
 filterOrder_Origin = [3, 4, 1, 4, 1, 4, 3, 4;
                       4, 2, 4, 3, 4, 3, 4, 2;
                       1, 4, 2, 4, 2, 4, 1, 4;
@@ -55,8 +59,10 @@ filterNames = {'r', 'g', 'b', 'w'};
 filterOrder = filterOrder_Origin;
 save(fullfile(rgbcrootpath, 'data', 'cfa', name), 'comment', 'data', 'filterNames', 'filterOrder', 'wavelength');
 
+%% CFA1: flipped and shifted repetition block 
+% for example raw images originally provided by OmniVision
 name = 'rgbc-omv1';
-comment = 'Flipped and shifted Omnivision RGBC CFA for raw images povided by Omnivation';
+comment = 'Flipped and shifted Omnivision RGBC CFA for raw images povided by OmniVision';
 data = [r, g, b, w];
 filterNames = {'r', 'g', 'b', 'w'};
 filterOrder = filterOrder_Origin; % starting from origin
@@ -65,6 +71,8 @@ tmp = repmat(filterOrder, [2, 2]); % replicate twice in row and col
 filterOrder = tmp(5 : 12, 4 : 11); % shift
 save(fullfile(rgbcrootpath, 'data', 'cfa', name), 'comment', 'data', 'filterNames', 'filterOrder', 'wavelength');
 
+%% CFA1: flipped and shifted repetition block 
+% for example raw images originally captured at Stanford (by Nathan)
 name = 'rgbc-omv2';
 comment = 'Flipped and shifted Omnivision RGBC CFA for raw images captured at Stanford';
 data = [r, g, b, w];
