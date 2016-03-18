de611d5b107ff4fd47854206dfdecc3e70e83840
qytian <qytian@stanford.edu>
2016-03-18 16:57:57 -0700

    change training conditions

--- scripts/s_rgbcRender.m
+++ scripts/s_rgbcRender.m
@@ -14,7 +14,7 @@ ieInit;
 %% L3
 % L3_rgbc-omv1 for omv example images
 % L3_rgbc-omv2 for stanford images
-fpL3 = fullfile(rgbcrootpath, 'data', 'l3', 'L3_rgbc-omv2.mat');
+fpL3 = fullfile(rgbcrootpath, 'data', 'l3', 'L3_rgbc-omv1.mat');
 load(fpL3);
 camera = l3d.get('camera');
 cfa = cameraGet(camera, 'sensor cfa pattern');
@@ -22,15 +22,15 @@ cfa = cameraGet(camera, 'sensor cfa pattern');
 %% Scenes on Scarlet
 dpScarlet = 'http://scarlet.stanford.edu/validation/SCIEN/L3/rgbc';
 
-% % for omv example images
-% scenes = {'omv'};
-% szRaw = [3264, 2448];
+% for omv example images
+scenes = {'omv'};
+szRaw = [3264, 2448];
 
 % for stanford images
-szRaw = [4224, 3136];
-scenes = {'Equad_path', 'HMJ_office_1', 'HMJ_office_2', 'cro_court', 'memchu1', 'memchu1up', ...
-           'memchu1up2', 'memchu2macbeth', 'memchu3', 'memchudoor', 'quad_arcade', 'quad_arch', ...
-           'quad_light', 'sap_tower', 'tree_round'};
+% szRaw = [4224, 3136];
+% scenes = {'Equad_path', 'HMJ_office_1', 'HMJ_office_2', 'cro_court', 'memchu1', 'memchu1up', ...
+%            'memchu1up2', 'memchu2macbeth', 'memchu3', 'memchudoor', 'quad_arcade', 'quad_arch', ...
+%            'quad_light', 'sap_tower', 'tree_round'};
        
 
 %% Render
--- scripts/s_rgbcTrain.m
+++ scripts/s_rgbcTrain.m
@@ -13,7 +13,7 @@ ieInit;
 
 %%
 dpCfa = fullfile(rgbcrootpath, 'data', 'cfa');
-cfas = {'rgbc-omv1.mat', 'rgbc-omv2.mat'};
+cfas = {'rgbc-omv2.mat', 'rgbc-omv1.mat'};
 
 for ii = 1 : length(cfas)
     %% Create camera
@@ -29,26 +29,34 @@ for ii = 1 : length(cfas)
     % illuminant levels are the brightness of the scene (cd/m2). If the camera
     % is in auto-exposure mode, the exposure time is determined by the first
     % entry in the illuminantLev list
-    l3d.illuminantLev = [1 5 10 20 30 40 50 60 70 80 90];
+    l3d.illuminantLev = [0.1 1 5 10 20 30 40 50 60 70 80 90];
     l3d.inIlluminantSPD = {'D65'};
     l3d.outIlluminantSPD = {'D65'};
     
     %% L3 training
     tic
     l3t = l3TrainRidge();
+    
     patchSize = [11 11];
     l3t.l3c.patchSize = patchSize;
-    l3t.l3c.cutPoints = {logspace(-2, -.6, 40), 1/32};
+
+    vSwing = cameraGet(l3d.camera, 'pixel voltage swing');
+    linearThresholds = logspace(log10(0.01), log10(vSwing * 0.95), 40);
+    
+    contrast = 1/32;
+    
+    vSat = 0.95 * vSwing;
+    satThresholds = {vSat, vSat, vSat, vSat};
+
+    l3t.l3c.cutPoints = {linearThresholds, contrast, satThresholds};
     l3t.train(l3d);
     toc
     
     %% Save L3
+    l3t.l3c.clearData() % clear training data
     fpSave = fullfile(rgbcrootpath, 'data', 'l3', ['L3_' l3d.camera.name '.mat']);
-    save(fpSave, 'l3d', 'l3t', '-v7.3');
-    
+    save(fpSave, 'l3d', 'l3t', '-v7.3');    
 end
-
-%%
 
 
 
